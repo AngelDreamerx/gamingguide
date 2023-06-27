ea7c3ff27b4f775ce3858f70137ba85961bbcddd
Luca Asmus <lucaasmus@icloud.com>
2023-06-27 18:52:52 +0200

    finish glossary mvp

--- src/components/Glossary.svelte
+++ src/components/Glossary.svelte
@@ -10,37 +10,60 @@
             language: "English"
         },
         {
+            title: "EEE",
+            description: "test",
+            language: "Test"
+        },
+        {
             title: "FPS",
             description: "(engl. „frames per second“) Steht für die Anzahl der Bilder",
             language: "English"
         }
     ]
 
-    let isSorted = 0 // 0 is not sorted, 1 is sorted down, 2 is sorted up
-    const toggleSort = () => {
-        if(isSorted === 0) {
-            isSorted = 1
-        } else if(isSorted === 1) {
-            isSorted = 2
+    let sortedWords = [...words] // variable to be able to reset to constant words array
+
+    let sortAscending = null
+    let searchQuery = ""
+    
+    let expandedItemIndex = 0
+
+    import { afterUpdate } from 'svelte';
+
+    let filteredWords = [];
+
+    afterUpdate(() => {
+        filteredWords = sortedWords.filter(item =>
+            item.title.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+    });
+    
+    function toggleSort() {
+        if (sortAscending === true) {
+            // set back to unsorted after ascending
+            sortAscending = null;
+            sortedWords = [...words];
+        } else if (sortAscending === false) {
+            // sort words ascending after descending
+            sortAscending = true;
+            sortedWords = [...words].sort((a, b) =>
+                b.title.toLowerCase().localeCompare(a.title.toLowerCase())
+            );
         } else {
-            isSorted = 0
+            // Sort in ascending order
+            sortAscending = false;
+            sortedWords = [...words].sort((a, b) =>
+              a.title.toLowerCase().localeCompare(b.title.toLowerCase())
+            );
         }
     }
-    
-    let search = ""
-    
-    // TODO: broken
-    const getFilteredAndSortedWords = () => {
-        let newWords = [...words]
-        if(isSorted === 1) {
-            newWords = newWords.sort((a, b) => a.title - b.title)
-        } else if (isSorted === 2) {
-            newWords = newWords.sort((a, b) => b.title - a.title)
+
+    function expandItem(index) {
+        if(index === expandedItemIndex) {
+            expandedItemIndex = -1
+        } else {
+            expandedItemIndex = index
         }
-        if(search) {
-            newWords = newWords.filter((value) => value === search)
-        }
-        return newWords
     }
 </script>
 
@@ -51,7 +74,7 @@
                 <div class="absolute left-2 top-[50%] -translate-y-[50%] h-4 w-4 text-sky-600 text-opacity-70">
                     <Search /> 
                 </div>
-                <input bind:value={search} placeholder="Suche" class="text-sky-600 w-48 bg-sky-800 bg-opacity-10 pl-8 pr-2 py-1 rounded" />
+                <input bind:value={searchQuery} placeholder="Suche" class="text-sky-600 w-48 bg-sky-800 bg-opacity-10 pl-8 pr-2 py-1 rounded" />
             </div>
 
             <button 
@@ -59,32 +82,47 @@
                 on:click={() => toggleSort()}
             >
                 <div class="w-6 h-6 text-sky-500"> 
-                    {#if isSorted === 0}
+                    {#if sortAscending === true}
+                        <ArrowUp />
+                    {:else if sortAscending === false}
+                        <ArrowDown /> 
+                    {:else}
                         <div class="w-full h-full text-opacity-20 text-slate-700">
                             <ArrowDown />
                         </div>
-                    {:else if isSorted === 1}
-                        <ArrowDown /> 
-                    {:else}
-                        <ArrowUp />
                     {/if}
                 </div>
                 Alphabetisch
             </button>
         </li>
 
-        {#each getFilteredAndSortedWords() as word}
+        {#each filteredWords as word, index}
         <li class="flex gap-4">
             <div class="bg-white p-4 rounded w-48">
                 <h6 class="text-3xl text-sky-600 font-semibold">{word.title}</h6>
-                <p class="mt-2 text-sm text-slate-500">{word.language}</p>
+                {#if index === expandedItemIndex}
+                    <p class="mt-2 text-sm text-slate-500">{word.language}</p>
+                {/if}
             </div>
-            <div class="bg-white p-4 rounded w-full">
-                <p class="text-sm text-slate-500">
-                    {word.description}
-                </p>
+            <div class="bg-white p-4 rounded w-full flex gap-4">
+                <button class="h-6 w-6 flex-shrink-0 text-sky-500" on:click={() => expandItem(index)}>
+                   {#if index === expandedItemIndex} 
+                        <ArrowDown/>
+                   {:else}
+                        <ArrowUp/>
+                   {/if} 
+                </button>
+                <div>
+                    <p class={`text-md text-slate-500 ${index === expandedItemIndex ? "line-clamp-none" : "line-clamp-1"}`}>
+                        {word.description}
+                    </p>
+                </div>
             </div>
         </li>
         {/each}
     </ul>
 </div>
+
+<style>
+
+</style>
