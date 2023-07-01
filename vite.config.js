cdf8161264dcece929527cd1a239d634f47b7f3e
Luca Asmus <lucaasmus@icloud.com>
2023-07-01 17:42:07 +0200

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "/gamingguide/",
   resolve: {
     alias: {
       src: "/src",
