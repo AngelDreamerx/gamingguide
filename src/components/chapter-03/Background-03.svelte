f3d14a4042eda2cb9bcf355cf2245cf193a92af3
Luca Asmus <lucaasmus@icloud.com>
2023-07-01 22:23:53 +0200

    fix chapter background order

--- src/components/chapter-03/Background-03.svelte
+++ src/components/chapter-03/Background-03.svelte
@@ -1,33 +1,30 @@
+<script>
+  import bg from "src/assets/backgrounds/cave/HG_cave_Hintergrund.png"
+</script>
+
 <div class="absolute w-screen h-screen inset-0 z-30 overflow-hidden">
-    <div class="fg-image h-screen bg-cover"/>
-</div>
-<div class="absolute w-screen h-screen inset-0 z-10 overflow-hidden">
-    <div class="bg-image h-screen bg-cover"/>
+    <div class="z-10 fg-image h-screen bg-cover"/>
 </div>
 <div class="absolute w-screen h-screen inset-0 z-20 overflow-hidden">
-    <div class="cloud-image h-screen bg-cover"/>
+    <div class="bg-image h-screen bg-cover"/>
+</div>
+<div class="absolute w-screen h-screen inset-0 z-0 overflow-hidden">
+    <img class="absolute w-full h-full" src={bg} alt="bg" />
 </div>
 
 <style>
-    .cloud-image {
-      width: 200%;
-      background-image: url('src/assets/backgrounds/mountain/HG_mountain_Vogel_Wolken.png');
-      background-repeat: repeat-x;
-      animation: slideLeft 150s linear infinite;
-    }
-
     .fg-image {
       width: 200%;
-      background-image: url('src/assets/backgrounds/mountain/HG_mountain_Boden.png');
+      background-image: url('src/assets/backgrounds/cave/HG_cave_Boden.png');
       background-repeat: repeat-x;
-      animation: fgSlideLeft 100s linear infinite;
+      animation: fgSlideRight 100s linear infinite;
     }
   
     .bg-image {
       width: 200%;
-      background-image: url('src/assets/backgrounds/mountain/HG_mountain.png');
+      background-image: url('src/assets/backgrounds/cave/HG_cave.png');
       background-repeat: repeat-x;
-      animation: bgSlideLeft 300s linear infinite;
+      animation: bgSlideLeft 200s linear infinite;
     }
   
     @keyframes bgSlideLeft {
@@ -39,16 +36,7 @@
       }
     }
 
-    @keyframes fgSlideLeft {
-      from {
-        background-position: 0 0;
-      }
-      to {
-        background-position: -4320px 0;
-      }
-    }
-
-    @keyframes slideLeft {
+    @keyframes fgSlideRight {
       from {
         background-position: 0 0;
       }
--- src/components/chapter-04/Background-04.svelte
+++ src/components/chapter-04/Background-04.svelte
@@ -1,30 +1,33 @@
-<script>
-  import bg from "src/assets/backgrounds/cave/HG_cave_Hintergrund.png"
-</script>
-
 <div class="absolute w-screen h-screen inset-0 z-30 overflow-hidden">
-    <div class="z-10 fg-image h-screen bg-cover"/>
+    <div class="fg-image h-screen bg-cover"/>
 </div>
-<div class="absolute w-screen h-screen inset-0 z-20 overflow-hidden">
+<div class="absolute w-screen h-screen inset-0 z-10 overflow-hidden">
     <div class="bg-image h-screen bg-cover"/>
 </div>
-<div class="absolute w-screen h-screen inset-0 z-0 overflow-hidden">
-    <img class="absolute w-full h-full" src={bg} alt="bg" />
+<div class="absolute w-screen h-screen inset-0 z-20 overflow-hidden">
+    <div class="cloud-image h-screen bg-cover"/>
 </div>
 
 <style>
+    .cloud-image {
+      width: 200%;
+      background-image: url('src/assets/backgrounds/mountain/HG_mountain_Vogel_Wolken.png');
+      background-repeat: repeat-x;
+      animation: slideLeft 150s linear infinite;
+    }
+
     .fg-image {
       width: 200%;
-      background-image: url('src/assets/backgrounds/cave/HG_cave_Boden.png');
+      background-image: url('src/assets/backgrounds/mountain/HG_mountain_Boden.png');
       background-repeat: repeat-x;
-      animation: fgSlideRight 100s linear infinite;
+      animation: fgSlideLeft 100s linear infinite;
     }
   
     .bg-image {
       width: 200%;
-      background-image: url('src/assets/backgrounds/cave/HG_cave.png');
+      background-image: url('src/assets/backgrounds/mountain/HG_mountain.png');
       background-repeat: repeat-x;
-      animation: bgSlideLeft 200s linear infinite;
+      animation: bgSlideLeft 300s linear infinite;
     }
   
     @keyframes bgSlideLeft {
@@ -36,7 +39,16 @@
       }
     }
 
-    @keyframes fgSlideRight {
+    @keyframes fgSlideLeft {
+      from {
+        background-position: 0 0;
+      }
+      to {
+        background-position: -4320px 0;
+      }
+    }
+
+    @keyframes slideLeft {
       from {
         background-position: 0 0;
       }
