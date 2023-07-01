51aa4a88ec7fb1197a8ee474c9bd48fb107d9caa
Luca Asmus <lucaasmus@icloud.com>
2023-07-01 21:43:01 +0200

    fix sun

--- src/components/main-menu/MainMenuBackground.svelte
+++ src/components/main-menu/MainMenuBackground.svelte
@@ -5,7 +5,7 @@
     <div class="bg-image h-screen bg-cover"/>
 </div>
 <div class="absolute w-screen h-screen inset-0 z-0 overflow-hidden">
-    <img class="absolute bottom-20" src="src/assets/backgrounds/sky/HG_sky_Sonne.png" alt="Sun" />
+    <img class="absolute bottom-20 w-full h-auto" src="src/assets/backgrounds/sky/HG_sky_Sonne.png" alt="Sun" />
 </div>
 
 <style>
