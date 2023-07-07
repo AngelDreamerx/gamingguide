d4e96d5f34ac2f7ddad36070e355d6b128d76bac
AngelDreamerx <angelika.laupheim@web.de>
2023-07-07 07:29:13 +0200

    Loading Problem fixed

--- loader.js
+++ loader.js
@@ -1,5 +1,8 @@
-var loader = document.querySelector(".loader")
+var loader = document.querySelector('.loader');
 
-
-setTimeout(() => { 
-  loader.classList.add("disppear")}, 5000);
+setTimeout(() => {
+  loader.classList.add('disppear');
+  setTimeout(() => {
+    loader.remove();
+  }, 300); // Remove the loader element after 0.3 seconds (same duration as the animation)
+}, 5000);
