2674c043374fa5af08b503f9cce9a285778d0df3
AngelDreamerx <angelika.laupheim@web.de>
2023-07-07 07:47:52 +0200

    Loader Deleted

--- loader.js
+++
@@ -1,8 +0,0 @@
-var loader = document.querySelector('.loader');
-
-setTimeout(() => {
-  loader.classList.add('disppear');
-  setTimeout(() => {
-    loader.remove();
-  }, 300); // Remove the loader element after 0.3 seconds (same duration as the animation)
-}, 5000);
